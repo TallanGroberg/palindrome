c3c2ad4003758c5e2a1ba4fab9d53334f93c833d
talon scott <tallan.taven@icloud.com>
2019-04-27 19:52:35 -0600

    palindrome passes all npm tests

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 module.exports = Phrase;
 
-// Adds `reverse` to all strings.
-String.prototype.reverse = function() {
-  return Array.from(this).reverse().join("");
+function reverse(string) {
+  return Array.from(string).reverse().join("");
 }
+
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
+
+  // Returns the letters in the content.
+this.letters = function letters() {
+  return this.content;    // stub return value
+}
+// Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+this.letters = function() {
+let theLetters = [];
+for (let i = 0; i < this.content.length; ++i) {
+  if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+    theLetters.push(this.content.charAt(i));
+  }
+}
+return theLetters.join("");
+}
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
@@ -15,6 +32,7 @@ function Phrase(content) {
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
+
     return this.processedContent() === reverse(this.processedContent());
   }
 }
